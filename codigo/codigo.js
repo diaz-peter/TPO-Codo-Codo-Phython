d12e36454f9a7da602ba5a4fc5acee98185d0f03
Peter Diaz <dpeter999@gmail.com>
2022-10-25 22:38:51 -0300

    Haciendo validaciones del formulario

--- codigo/codigo.js
+++ codigo/codigo.js
@@ -1,29 +1,42 @@
-const btnEnviar = document.getElementById('btn-enviar');
 const nombre = document.getElementById("name");
-const email = document.getElementById("email");
+const correo = document.getElementById("email");
+const mensaje = document.getElementById("mesagge");
+const telefono = document.getElementById("phone");
+const form = document.getElementById("form");
+const parrafo = document.getElementById("warnings");
 
-form.addEventListener("submit", e=>{
+form.addEventListener("submit", e => {
     e.preventDefault();
+
     let warnings = "";
-    let entrar = false;
+    let enviado = false;
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+
     parrafo.innerHTML = "";
-    if(nombre.value.length <6) {
-        warnings += `-El nombre debe tener minimo 6 caracteres <br>`;
-        entrar = true;
-    }
-    if(!regexEmail.test(email.value)) {
-    warnings += `-El email no es valido <br>`;
-    entrar = true;
-    }
-    if(pass.value.length <8) {
-    warnings += `-La contraseña no es valida <br>`;
-    entrar = true;
+
+    if (nombre.value.length < 3) {
+        warnings += `-El nombre debe tener minimo 3 caracteres <br>`;
+        enviado = true;
     }
 
-    if(entrar) {
+    if (!regexEmail.test(correo.value)) {
+        warnings += `-El email no es valido <br>`;
+        enviado = true;
+    }
+
+    if (mensaje.value.length < 20) {
+        warnings += `-El mensaje no puede tener menos de 20 caracteres <br>`;
+        enviado = true;
+    }
+
+    if (telefono.value.length < 10) {
+        warnings += `-El telefono no puede tener menos de 10 caracteres <br>`;
+        enviado = true;
+    }
+
+    if (enviado) {
         parrafo.innerHTML = warnings;
-    }else
-        parrafo.innerHTML = "Enviado";
+    } else
+        parrafo.innerHTML = "Tu mensaje ha sido enviado, muchas gracias. <br> <h3>Aguante El Ciclon¡¡</h3>";
 
 });
